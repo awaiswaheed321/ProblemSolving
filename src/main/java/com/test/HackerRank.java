da4555655eb13ea118fdb9ecb9482c2865ff5a62
Awais Waheed <awaiswaheed321@gmail.com>
2025-04-02 10:55:48 -0500

    Problems

--- src/main/java/com/test/HackerRank.java
+++ src/main/java/com/test/HackerRank.java
@@ -234,4 +234,14 @@ public class HackerRank {
         }
         return str.toString();
     }
+
+    public static int camelcase(String s) {
+        int count = 1;
+        for(char c: s.toCharArray()) {
+            if(c >=65 && c<=90) {
+                count++;
+            }
+        }
+        return count;
+    }
 }
