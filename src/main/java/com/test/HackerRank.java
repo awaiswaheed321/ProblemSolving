d8385ac541fa1e38e59ed3cb51615663ea0b5fa2
Awais Waheed <awaiswaheed321@gmail.com>
2025-04-03 19:32:19 -0500

    Problems

--- src/main/java/com/test/HackerRank.java
+++ src/main/java/com/test/HackerRank.java
@@ -276,4 +276,15 @@ public class HackerRank {
         return "pangram";
     }
 
+    public int removeElement(int[] nums, int val) {
+        int k = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != val) {
+                nums[k] = nums[i];
+                k++;
+            }
+        }
+        return k;
+    }
 }
