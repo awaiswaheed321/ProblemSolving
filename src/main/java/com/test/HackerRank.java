1be26ec6b9cf9b8a6d7164665e20d3c6de31c011
Awais Waheed <awaiswaheed321@gmail.com>
2025-04-07 12:26:54 -0500

    Problems

--- src/main/java/com/test/HackerRank.java
+++ src/main/java/com/test/HackerRank.java
@@ -338,4 +338,20 @@ public class HackerRank {
         Arrays.sort(nums);
         return nums[nums.length/2];
     }
+
+    public int lengthOfLongestSubstring(String s) {
+        int i=0, j=0, max = 0;
+        HashSet<Character> set = new HashSet<>();
+        while(j < s.length()){
+            if(set.contains(s.charAt(j))) {
+                set.remove(s.charAt(i));
+                i++;
+            } else {
+                set.add(s.charAt(j));
+                j++;
+                max = Math.max(max, set.size());
+            }
+        }
+        return max;
+    }
 }
