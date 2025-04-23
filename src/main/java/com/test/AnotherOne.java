f0ae40309f6606beb4935f1f27148e9775bac4ef
Awais Waheed <awaiswaheed321@gmail.com>
2025-04-23 12:18:25 -0500

    Problems

--- src/main/java/com/test/AnotherOne.java
+++ src/main/java/com/test/AnotherOne.java
@@ -1,5 +1,6 @@
 package com.test;
 
+import java.util.List;
 import java.util.Stack;
 
 public class AnotherOne {
@@ -9,6 +10,23 @@ public class AnotherOne {
         System.out.println(simplifyPath("/../"));
         System.out.println(simplifyPath("/.../a/../b/c/../d/./"));
         System.out.println(simplifyPath("/home/"));
+    }
+
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode res = new ListNode(0);
+        ListNode current = res;
+        int carry  = 0;
+        while(l1 != null || l2 != null || carry != 0) {
+            int x = l1 == null ? 0 : l1.val;
+            int y = l2 == null ? 0: l2.val;
+            int sum = x + y + carry;
+            carry = sum/10;
+            current.next = new ListNode(sum%10);
+            current = current.next;
+            if(l1 != null) l1 = l1.next;
+            if(l2 != null) l2 = l2.next;
+        }
+        return res.next;
     }
 
     public static String simplifyPath(String path) {
